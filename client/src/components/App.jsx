417dcb98fa2e5b1147923d6f6369e0ce2521f9ee
Jake Perez <jacobanthony.perez@gmail.com>
2018-01-13 23:17:34 -0800

    [ops] - more file structure

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>HERE IS THE REACT APP!!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
