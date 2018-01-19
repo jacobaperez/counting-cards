0e886bdce5185279e9a510797f428542719c1956
Jake Perez <jacobanthony.perez@gmail.com>
2018-01-18 18:07:55 -0800

    [ops] - work on loading css files - work on resolving .js and .jsx files

--- client/src/components/HiLo/HiLo.jsx
+++ client/src/components/HiLo/HiLo.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import './HiLo.css';
 
 class HiLo extends React.Component {
   constructor() {
     super();
+    this.state = {
+      count: 0,
+      cardsRemaining: 0
+    }
   }
 
   render () {
     return (
       <div>
         <h1>HI LO CARD COUNTING SYSTEM</h1>
+        <div>
+          <h2>Card Element goes here.</h2>
+        </div>
+        <div className="action-buttons">
+          <button type="button" className="btn btn-primary">Draw Card</button>
+          <button type="button" className="btn btn-primary">Shuffle Deck</button>
+          <button type="button" class="btn btn-primary">Running Count</button>
+          <button type="button" class="btn btn-primary">True Count</button>
+        </div>
+        <div>
+          <button type="button" class="btn btn-primary">Home</button>
+        </div>
 
       </div>
     )
